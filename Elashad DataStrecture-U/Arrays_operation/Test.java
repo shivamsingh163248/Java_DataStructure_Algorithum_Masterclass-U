df450ad1e789b8955f788aa3d9b1f9575c98f424
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-03-21 23:46:09 +0530

    learning arrays in java

--- Elashad DataStrecture-U/Arrays_operation/Test.java
+++ Elashad DataStrecture-U/Arrays_operation/Test.java
@@ -3,7 +3,15 @@ package Arrays_operation;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// create the values 
+		Arrays login = new Arrays(6);
+		login.insert(5, 6);
+		login.insert(3, 3);
+		login.insert(10, 2);
+		login.insert(5, 6);
+		
+		
 
 	}
 
