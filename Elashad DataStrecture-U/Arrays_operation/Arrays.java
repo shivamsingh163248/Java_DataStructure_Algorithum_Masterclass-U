27c251ff29d0cbddf470c0b7292ba5c020118849
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-03-21 20:48:58 +0530

    update

--- Elashad DataStrecture-U/Arrays_operation/Arrays.java
+++ Elashad DataStrecture-U/Arrays_operation/Arrays.java
@@ -2,8 +2,41 @@ package Arrays_operation;
 
 public class Arrays {
 
-	int[]arr = null  ; 
-	
+	private int[]arr = null  ; 
 	// crating the constructor of the arrays instation 
+	public Arrays(int size) {
+		// hear 
+		arr = new int[size] ; 
+		// and insert  the values 
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.MIN_VALUE ; 
+		}
+	}
+	
+	// creating the insert function 
+	public void insert(int index , int value) {
+	
+		
+		try {
+			// checking the condition
+			if (arr[index] == Integer.MIN_VALUE) {
+				// insert values 
+				arr[index] = value ;
+				System.out.println("Successful inserted ");
+				
+			}else {
+				System.out.println("shell is alrady accoupide ");
+			}
+			
+		} catch (ArrayIndexOutOfBoundsException e) {
+		       System.out.println("out of bond exception ");
+		}
+		
+		
+	}
+	
+	
+	
+
 
 }
