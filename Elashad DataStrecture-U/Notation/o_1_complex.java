dd38c8f66c7578cc50367707e4550b64e3b473c8
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-03-21 14:13:00 +0530

    time complexcity

---
+++ Elashad DataStrecture-U/Notation/o_1_complex.java
@@ -0,0 +1,17 @@
+package Notation;
+
+public class o_1_complex {
+	
+	// creating the static function to solved 
+	public static int square(int n ) {
+		return n*n ; 
+	}
+	public static void main(String[] args) {
+		
+		// calling the function with the consent time complexity 
+		System.out.println(square(6));
+		
+		
+	}
+
+}
