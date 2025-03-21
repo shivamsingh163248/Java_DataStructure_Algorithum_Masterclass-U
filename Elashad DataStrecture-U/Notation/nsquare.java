c2af96fe35bfa6ac4a22e76738f0472c1b3f0e81
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-03-21 14:51:08 +0530

    n2 notation

---
+++ Elashad DataStrecture-U/Notation/nsquare.java
@@ -0,0 +1,16 @@
+package Notation;
+
+public class nsquare {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+		
+			for (int j = 0; j < 10; j++) {
+				
+				System.out.println(j+"   "+i);
+				
+			}
+		}
+	}
+
+}
