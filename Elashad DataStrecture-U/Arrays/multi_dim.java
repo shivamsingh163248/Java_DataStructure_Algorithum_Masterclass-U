23184fe8906bebad35e22a62da6d1e2f70cdfa24
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-03-21 18:43:30 +0530

    update

--- Elashad DataStrecture-U/Arrays/multi_dim.java
+++ Elashad DataStrecture-U/Arrays/multi_dim.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class multi_dim {
 	
 	
@@ -23,6 +25,60 @@ public class multi_dim {
 		for (int i = 0; i < arr.length; i++) {
 		     System.out.println(arr[i].length);	
 		}
+		
+		// now values store 
+		
+	}
+	
+	// creating the function to take a input 2d arryas 
+	public static void print(int[][]arr) {
+		
+		// creating the two loop for the iterating arrays 
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] +" " );
+	
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	
+	public static int[][] arr_2d(){
+		
+		// taking the input from the user 
+		//declerd variable 
+		Scanner input = new Scanner(System.in) ; 
+		System.out.println("enter the size ");
+		int size = input.nextInt() ; 
+		
+		// creating the loop for the creating the jacked arrays according the user input 
+		// creating the arrays 
+		int[][] arr = new int[size][] ; 
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			// now creating size of the every arrys 
+			System.out.println("Enter the size of id arrys at index : "+" "+i);
+			int su_size = input.nextInt() ; 
+			arr[i] = new int[su_size] ; 
+		}
+		return arr ; 
+	}
+	
+	public static int[][] set_values(int[][]arr) {
+		
+		Scanner input = new Scanner(System.in) ; 
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.println("Enter the value at index"+" "+i+" "+j);
+				int num = input.nextInt() ; 
+				arr[i][j] = num ; 
+			}
+		}
+		return arr ;
 	}
 
 	public static void main(String[] args) {
