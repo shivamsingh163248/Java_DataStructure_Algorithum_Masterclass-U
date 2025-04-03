a2a8d64f99f6ac695eee47a200e8136f93834dd1
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-04-04 01:37:22 +0530

    update

--- Elashad DataStrecture-U/Arrayes_2d/Arrays_Integer.java
+++ Elashad DataStrecture-U/Arrayes_2d/Arrays_Integer.java
@@ -1,11 +1,10 @@
 package Arrayes_2d;
 
 public class Arrays_Integer {
-	
 	// creating the string
 	int[][] arr ; 
-	public Arrays_Integer() {
-		
+	public Arrays_Integer(int size) {
+		arr = new int[size][]  ;
 	}
 
 }
