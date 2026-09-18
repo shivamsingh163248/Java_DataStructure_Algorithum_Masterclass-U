559f279cde1304e5bc4108218c1ea0838e4e315f
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2026-09-19 00:37:04 +0530

    update and test something

---
+++ Elashad DataStrecture-U/Notation/bigo.java
@@ -0,0 +1,16 @@
+package Notation;
+
+public class bigo {
+
+    // now i am creating a one static function and then we are the going to use in the main funtion
+    public static  void add(){
+        System.out.println("this is just for the test purpose") ;
+
+
+    }
+
+    // now i am going to create the main fuction and call the this function and use it
+    public static void main(String[] args) {
+        add();
+    }
+}
